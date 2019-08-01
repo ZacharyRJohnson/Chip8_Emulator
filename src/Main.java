8a687d983ba1bbb51f854ecb325add347dec4d75
Zachary <zachjohnson1900@gmail.com>
2019-08-01 19:14:54 -0400

    Beginning working on GUI

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) throws Exception{
         Chip8 chip8 = new Chip8();
-        chip8.executeOpcode();
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(15, 12, 15, 12));
+        chip8.getDisplay().drawScreen();
+        //Display screen = new Display();
+        //screen.drawScreen();
+        Button btn = new Button();
+        btn.setText("Test");
+
+        hBox.getChildren().add(chip8.getDisplay());
+        hBox.getChildren().add(btn);
+
+
+        Scene scene = new Scene(hBox);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Chip-8 Emulator");
+        primaryStage.show();
+        //chip8.executeOpcode();
     }
 
     public static void main(String[] args) {
